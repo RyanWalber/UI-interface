b60146788e12059f12d5400fb93ad87ca99cc594
RyanWalber <144794932+RyanWalber@users.noreply.github.com>
2025-07-16 16:30:29 -0300

    1 dia

--- UI/Assets/Scripts/MenuManager.cs
+++ UI/Assets/Scripts/MenuManager.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private string Jornal;
+
+
     public void NewGame()
     {
-        SceneManager.LoadScene("Jornal");
+        SceneManager.LoadScene(Jornal);
     }
 }
